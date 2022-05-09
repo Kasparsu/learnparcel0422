96a3c79a1b47b6a7475d79d30b02c7150b0c08a9
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-05-09 20:13:17 +0300

    Clear App component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
     <div class="container">
-        <button class="button" @click="modalActive=true">Open Modal</button>
-        <button class="button" @click="modalActive2=true">Open Modal2</button>
-        <modal 
-        :active="modalActive"
-        @close="modalActive=false"
-        img="https://placekitten.com/1280/720"></modal>
-        <modal 
-        :active="modalActive2"
-        @close="modalActive2=false"
-        img="https://placebear.com/1280/720"></modal>
+
     </div>
 </template>
 
 <script>
-import Modal from './Modal.vue';
+
 export default {
-  components: { Modal },
+
     data() {
         return {
-            modalActive: false,
-            modalActive2: false,
+            
         }
     }
 }
