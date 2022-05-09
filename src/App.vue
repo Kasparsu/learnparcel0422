9650e267966ceea4619b71828fa7569513e4787b
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-05-09 19:05:19 +0300

    clear example

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,6 @@
 <template>
     <div class="container">
-        <div class="field has-addons">
-            <div class="control">
-                <input class="input" v-model="message" @keydown.enter="addItem">
-            </div>
-            <div class="control">
-                <button class="button is-primary" @click="addItem">
-                    Add Item
-                </button>
-            </div>
-        </div>
-        <div class="content">
-            <ul>
-                <li v-for="(item,index) in items" :key="index">
-                    {{item}}
-                    <button class="button is-primary is-small" @click="removeItem(index)">Done</button>
-                </li>
-            </ul>
-        </div>
+        
     </div>
 </template>
 
@@ -25,19 +8,7 @@
 export default {
     data() {
         return {
-            message: '',
-            items: ['piim', 'sai', 'leib']
-        }
-    },
-    methods: {
-        addItem(){
-            if(this.message.trim() != ''){
-                this.items.push(this.message);
-            }
-            this.message = '';
-        },
-        removeItem(index){
-            this.items.splice(index, 1);
+
         }
     }
 }
