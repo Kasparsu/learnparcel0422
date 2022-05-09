b4dd4bfc039b867fe8011b1cc0761f13849e11c1
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-05-09 20:59:08 +0300

    Api usage

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="container">
-
+        <h1 class="is-size-1">{{text}}</h1>
     </div>
 </template>
 
 <script>
-
+import axios from 'axios';
 export default {
-
+    created(){
+        axios.get('https://cat-fact.herokuapp.com/facts/random').then(res => {
+            console.log(res);
+            this.text = res.data.text
+        });
+    },
     data() {
         return {
-            
+            text:''
         }
     }
 }
